7f44efbba06d2facdd27d53abeb0ada904f124d0
ludo62 <fournier.ludovic@orange.fr>
2023-08-03 07:49:52 +0200

    bug fix page.tsx

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -4,9 +4,9 @@ export default function Navbar() {
     return (
         <nav className="bg-blue-800 p-4">
             <ul className="flex justify-evenly text-2xl font-bold">
-                <li><Link href="/">Home</Link></li>
-                <li><Link href="/api/auth/signin">Sign In</Link></li>
-                <li><Link href="/api/auth/signout">Sign Out</Link></li>
+                <li><Link href="/">Accueil</Link></li>
+                <li><Link href="/api/auth/signin">Connexion</Link></li>
+                <li><Link href="/api/auth/signout">Déconnexion</Link></li>
                 <li><Link href="/server">Server</Link></li>
                 <li><Link href="/client">Client</Link></li>
                 <li><Link href="/extra">Extra</Link></li>
