250947c136a65385b962f0f8361a5861e8cd6965
ludo62 <fournier.ludovic@orange.fr>
2023-08-03 07:31:59 +0200

    add SECRET_KEY to .env and config credentials

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,5 +1,36 @@
 import type { NextAuthOptions } from 'next-auth';
+import GitHubProvider from 'next-auth/providers/github';
+import CredentialProvider from 'next-auth/providers/credentials';
 
 export const options: NextAuthOptions = {
-    providers: [],
+    providers: [
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string,
+        }),
+        CredentialProvider({
+            name: 'Credentials',
+            credentials: {
+                username: {
+                    label: 'Username',
+                    type: 'text',
+                    placeholder: 'Pseudo',
+                },
+                password: {
+                    label: 'Password',
+                    type: 'password',
+                    placeholder: 'Mot de passe',
+                }
+            },
+            async authorize(credentials) {
+                const user = { id: '42', name: 'Ludovic', password: 'admin' };
+
+                if (credentials?.username === user.name && credentials?.password === user.password) {
+                    return user
+                } else {
+                    return null
+                }
+            }
+        })
+    ],
 }
